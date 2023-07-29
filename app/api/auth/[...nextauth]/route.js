df353fb8d72a3f5143831a61974e18f2502da67e
your-ehsan <ehsanshahid787@gmail.com>
2023-07-29 22:44:44 +0500

    mongo db added with google authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,44 +1,46 @@
 import User from "@models/user";
 import { ConnectToDB } from "@utils/database";
-import NextAuth from "next-auth/next";
+import nextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
 
-import Google from "next-auth/providers/google";
-
-const handler = NextAuth({
+const handler = nextAuth({
   providers: [
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
+  callbacks: {
 
-  async signIn({ profile }) {
-    try {
-      await ConnectToDB();
-      const userExist = await User.findOne({
-        email: profile.email,
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
-      if (!userExist) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
-      }
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    async signIn({ profile }) {
+      try {
+        await ConnectToDB();
+        const userExist = await User.findOne({
+          email: profile.email,
+        });
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
--- utils/database.js
+++ utils/database.js
@@ -4,17 +4,22 @@ let isConnected = false;
 
 const ConnectToDB = async () => {
   mongoose.set("strictQuery", true);
+
   if (isConnected) {
-    console.log("🎉🎊 MongoDB🍃 connected 🔥 ...");
+    console.log("MongoDB🍃 already connected 🔥");
     return;
   }
+
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "gpt-prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     isConnected = true;
+
+    console.log("🎉🎊 MongoDB🍃 connected 🔥 ...");
   } catch (error) {
     console.log("... mongoDB not connected 🚫", error);
   }

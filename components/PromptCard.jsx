b6ab757c12681884b7d6a2660d056042c347b0b3
your-ehsan <ehsanshahid787@gmail.com>
2023-08-20 01:02:51 +0500

    minor code refactoring

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
+import Image from "next/image";
 
 const PromptCard = ({
   handleEdit,
